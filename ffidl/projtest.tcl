0d0bc8df9087e3fe5207c55316ada00185b59eb5
peter dean <p.dean@internode.on.net>
2023-02-03 17:27:08 +1000

    sort of pipe

--- ffidl/projtest.tcl
+++ ffidl/projtest.tcl
@@ -196,4 +196,8 @@ set x [proj_inv $mga2gda $x]
 puts [lmap p $x {fmt3 $p}]
 
 
+set x {502810 6964520 50}
+puts [lmap p $x {fmt3 $p}]
+set x [proj_inv $mga2gda [proj_fwd $gda2ahd [ proj_fwd $mga2gda $x]]]
+puts [lmap p $x {fmt3 $p}]
 
