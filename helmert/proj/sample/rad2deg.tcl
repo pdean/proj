7e8dc2607728836ed6539ba8521ad2684479dc3b
peter dean <p.dean@internode.on.net>
2024-03-07 15:44:06 +1000

    las testing files

---
+++ helmert/proj/sample/rad2deg.tcl
@@ -0,0 +1,9 @@
+namespace path {::tcl::mathop ::tcl::mathfunc}
+
+set pi [acos -1.0]
+
+puts [/ 180.0 $pi]
+
+set x 4.84813681109536e-6
+
+puts [/ 1 $x]
--- helmert/proj/sample/testlas.sh
+++ helmert/proj/sample/testlas.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 las2las64 -i g.txt -iparse xyz \
-    -transform_affine 0.99960269575,1.55774140637E-04,-20.3070168478,-17.3858693056 \
+    -transform_affine 0.99960269575,5.6078691E-01,-20.3070168478,-17.3858693056 \
     -rescale 0.001 0.001 0.001 -stdout -otxt    
